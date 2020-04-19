bb52f89425b372fe8e51aa79b5a7940622a338b9
Nebz <28622481+NebzHB@users.noreply.github.com>
2020-04-19 19:03:43 +0200

    no compil python

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -20,24 +20,24 @@ sudo apt-get update
 echo 10 > ${PROGRESS_FILE}
 echo "--10%"
 echo "Installation des dépendances apt"
-cd ..
-cd ..
-cd plugins/aTVremote/resources/dependances_python/
+#cd ..
+#cd ..
+#cd plugins/aTVremote/resources/dependances_python/
 sudo apt-get -y install python3 python3-pip python3-setuptools build-essential libssl-dev libffi-dev python3-dev
-echo 20 > ${PROGRESS_FILE}
-echo "--20%"
-sudo curl -O https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tar.xz
-sudo tar -xf Python-3.6.9.tar.xz
-cd Python-3.6.9/
-echo 30 > ${PROGRESS_FILE}
-echo "--30%"
-./configure --enable-optimizations
-make -j 2
-sudo make install
+#echo 20 > ${PROGRESS_FILE}
+#echo "--20%"
+#sudo curl -O https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tar.xz
+#sudo tar -xf Python-3.6.9.tar.xz
+#cd Python-3.6.9/
+#echo 30 > ${PROGRESS_FILE}
+#echo "--30%"
+#./configure --enable-optimizations
+#make -j 2
+#sudo make install
 #sudo pip3 install aiohttp==3.0.1
 #echo 60 > ${PROGRESS_FILE}
-echo 80 > ${PROGRESS_FILE}
-echo "--80%"
+echo 50 > ${PROGRESS_FILE}
+echo "--50%"
 sudo pip3 install -I wheel
 sudo pip3 install -I git+https://github.com/NebzHB/pyatv@release_0_5_x
 sudo pip3 install --upgrade pip
