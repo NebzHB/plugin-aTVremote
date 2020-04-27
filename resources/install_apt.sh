594d3e73bf61f5963e120930640f4f0ba61aeab8
Nebz <28622481+NebzHB@users.noreply.github.com>
2020-04-27 13:25:23 +0200

    install master branch

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -198,7 +198,8 @@ try sudo virtualenv -p `which python3.7` ${BASEDIR}/atvremote/
 source <(sudo cat /var/www/html/plugins/aTVremote/resources/atvremote/bin/activate)
 
 try sudo `which pip3` install -I wheel
-try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@release_0_5_x
+#try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@release_0_5_x
+try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@master
 deactivate
 
 try sudo pip3 install --upgrade pip
