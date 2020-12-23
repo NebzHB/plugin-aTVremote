7fe1124a96b1acff754c65b8dbf4e9d3bb276062
Nebz <28622481+NebzHB@users.noreply.github.com>
2020-12-23 07:19:13 +0100

    new object list

--- desktop/php/aTVremote.php
+++ desktop/php/aTVremote.php
@@ -91,8 +91,8 @@ $eqLogics = eqLogic::byType('aTVremote');
 										<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
 											<option value="">{{Aucun}}</option>
 											<?php
-											foreach (jeeObject::all() as $object) {
-												echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+											foreach ((jeeObject::buildTree(null, false)) as $object) {
+												echo '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
 											}
 											?>
 										</select>
