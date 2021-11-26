7f32bc53f5f7852025ce015989c755b5641157a2
Nebz <28622481+NebzHB@users.noreply.github.com>
2021-11-26 14:41:53 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -64,8 +64,8 @@ try sudo pip3 install virtualenv
 try sudo virtualenv -p `which python3.7` ${BASEDIR}/atvremote/
 source <(sudo cat ${BASEDIR}/atvremote/bin/activate)
 
-try sudo `which pip3` install -I wheel
 try sudo `which pip3` install -I setuptools_rust
+try sudo `which pip3` install -I wheel
 #try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@release_0_5_x
 try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@master
 deactivate
