ce57d9392e5ec4b05470dc2c190fb76c43a3bc56
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-07-10 21:36:33 +0200

    don't change miniaudio and uses pip instead of pip3

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -57,7 +57,7 @@ try sudo apt-get update
 step 60 "Nettoyage anciens modules"
 cd ${BASEDIR};
 #remove old local modules
-silent sudo rm -rf node_modules
+silent sudo rm -fR node_modules
 silent sudo rm -f package-lock.json
 
 step 70 "Installation des librairies du démon, veuillez patienter svp"
@@ -66,47 +66,28 @@ silent sudo chown -R www-data:www-data .
 try sudo npm install --no-fund --no-package-lock --no-audit
 silent sudo chown -R www-data:www-data . 
 
-lsb_release -c | grep stretch
-if [ $? -eq 0 ]
-then
-  silent python3.7 --version
-  if [ $? -ne 0 ]; then
-    step 80 "Installation de python 3.7"
-    try sudo DEBIAN_FRONTEND=noninteractive apt-get install -y zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libreadline-dev curl
-    try sudo curl -O https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tar.xz
-    silent sudo tar -xf Python-3.7.9.tar.xz
-    #Clean tar file
-    silent sudo rm -fR Python-3.7.9.tar.xz
-    cd Python-3.7.9/
-    #sudo ./configure --enable-optimizations #too slow
-    try sudo ./configure --prefix=/usr
-    try sudo make -j 2
-    try sudo make altinstall
-    cd ..
-    #Clean Python folder
-    silent sudo rm -fR Python-3.7.9/
-  fi
-fi
-
 step 90 "Installation librairie atvremote"
 silent sudo rm -fR ${BASEDIR}/atvremote
 try sudo pip3 install virtualenv
+
 silent which python3.7
 if [ $? -eq 0 ];then
 	try sudo virtualenv -p `which python3.7` ${BASEDIR}/atvremote/
 else
-	try sudo virtualenv -p `which python3.9` ${BASEDIR}/atvremote/
+	silent which python3.9
+	if [ $? -eq 0 ];then
+		try sudo virtualenv -p `which python3.9` ${BASEDIR}/atvremote/
+	else
+		try sudo virtualenv -p `which python3` ${BASEDIR}/atvremote/
 fi
 source <(sudo cat ${BASEDIR}/atvremote/bin/activate)
 
-try sudo `which pip3` install -I wheel
-try sudo `which pip3` uninstall miniaudio
-try sudo `which pip3` install --no-binary :all: miniaudio==1.40
-#try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@release_0_5_x
-try sudo `which pip3` install -I git+https://github.com/NebzHB/pyatv@master
+try sudo `which pip` install -I wheel
+try sudo `which pip` install -I git+https://github.com/NebzHB/pyatv@master
+
 deactivate
 
-try sudo pip3 install --upgrade pip
+#try sudo pip3 install --upgrade pip
 try sudo ${BASEDIR}/atvremote/bin/python -m pip install --upgrade pip
 
 post
