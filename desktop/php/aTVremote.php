c02afef87e60977d8374f047e0ffcc910cc10d03
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-07-10 23:15:52 +0200

    Update aTVremote.php

--- desktop/php/aTVremote.php
+++ desktop/php/aTVremote.php
@@ -78,7 +78,7 @@ $eqLogics = eqLogic::byType('aTVremote');
 							<fieldset>
 								<div class="form-group">
 									<br/>
-									<label class="col-sm-3 control-label help" data-help="{{Renommez dans l'AppleTV > Réglages > Général > A Propos}}">{{Nom de l'appleTV}}</label>
+									<label class="col-sm-3 control-label help" data-help="{{Renommez dans l'AppleTV > Réglages > Général > A Propos puis relancez une Découverte}}">{{Nom de l'appleTV}}</label>
 									<div class="col-sm-4">
 										<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
 										<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'appleTV}}" readonly />
