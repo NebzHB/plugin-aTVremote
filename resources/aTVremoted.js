58bdf34aa20a03d8a63bb1422acc5a5e34d1db67
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-07-22 14:24:55 +0200

    send powerstate to jeedom

--- resources/aTVremoted.js
+++ resources/aTVremoted.js
@@ -106,24 +106,17 @@ function connectATV(mac,version) {
 			}
 			if(data != "pyatv> ") {
 				data=data.replace('\npyatv> ','');
-				/*
-				if(data.includes('PowerState.')) {
+				
+				if(data.match(/^[0-9]{0,3}\.[0-9]$/)) {
+					jsend({eventType: 'volume', data : data, mac: mac});
+				} else if(data.includes('PowerState.')) {
 					jsend({eventType: 'powerstate', data : data, mac: mac});
 				} else if(data.includes('App: ')) {
 					jsend({eventType: 'app', data : data, mac: mac});
-				} else {
-					jsend({eventType: 'hash', data : data, mac: mac});
-				} */
-				if(data.includes('Could not find any Apple TV on current network')) {
+				} else if(data.includes('Could not find any Apple TV on current network')) {
 					lastErrorMsg=data;
-				}
+				} 
 				
-				if(data.includes('App: ')) {
-					jsend({eventType: 'app', data : data, mac: mac});
-				}
-				if(data.match(/^[0-9]{0,3}\.[0-9]$/)) {
-					jsend({eventType: 'volume', data : data, mac: mac});
-				}
 				
 				Logger.log('cmd | '+data,LogType.DEBUG);
 			}
