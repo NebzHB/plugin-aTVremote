348889bba5b62fb4e7c8a668ef7121f445871c6f
Nebz <28622481+NebzHB@users.noreply.github.com>
2023-04-03 09:09:51 +0200

    Fix for 4.4

--- core/ajax/aTVremote.ajax.php
+++ core/ajax/aTVremote.ajax.php
@@ -35,6 +35,10 @@ try {
     throw new Exception(__('{{Aucune méthode correspondante à}} : ', __FILE__) . init('action'));
     /*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-    ajax::error(displayExeption($e), $e->getCode());
+	if(version_compare(jeedom::version(), '4.4', '>=')) {
+		ajax::error(displayException($e), $e->getCode());
+	} else {
+		ajax::error(displayExeption($e), $e->getCode());
+	}
 }
 ?>
