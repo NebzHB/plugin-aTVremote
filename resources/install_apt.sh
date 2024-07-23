2769a0f70550236a0f4ad6a783d309f20274cfd2
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-07-23 07:18:01 +0200

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -20,33 +20,63 @@ silent sudo chown -R www-data $(realpath $BASEDIR/..)
 lsb_release -c | grep jessie
 if [ $? -eq 0 ]
 then
-    echo "$HR"
-    echo "== KO == Erreur d'Installation"
-    echo "$HR"
-    echo "== ATTENTION Debian 8 Jessie n'est officiellement plus supportée depuis le 30 juin 2020, merci de mettre à jour votre distribution !!!"
-    exit 1
+	echo 1 > $TMPFOLDER/hasError.$$
+	echo -e "$HR" >> $TMPFOLDER/errorLog.$$ 
+	echo -e "== ATTENTION Debian 8 Jessie n'est officiellement plus supportée depuis le 30 juin 2020, merci de mettre à jour votre distribution !!!" >> $TMPFOLDER/errorLog.$$ 
+	post
+	exit 1
 fi
 
 #stretch is not supported because of old python
 lsb_release -c | grep stretch
 if [ $? -eq 0 ]
 then
-    echo "$HR"
-    echo "== KO == Erreur d'Installation"
-    echo "$HR"
-    echo "== ATTENTION Debian 9 Stretch n'est pas supporté car la version de python est trop vieille, merci de mettre à jour votre distribution !!!"
-    exit 1
+	echo 1 > $TMPFOLDER/hasError.$$
+	echo -e "$HR" >> $TMPFOLDER/errorLog.$$ 
+	echo -e "== ATTENTION Debian 9 Stretch n'est officiellement plus supportée depuis le 30 juin 2022, merci de mettre à jour votre distribution !!!" >> $TMPFOLDER/errorLog.$$ 
+	post
+	exit 1
 fi
 
 #stretch is not supported because of old python
 lsb_release -c | grep buster
 if [ $? -eq 0 ]
 then
-    echo "$HR"
-    echo "== KO == Erreur d'Installation"
-    echo "$HR"
-    echo "== ATTENTION Debian 10 Buster n'est pas supporté car la version de python est trop vieille, merci de mettre à jour votre distribution !!!"
-    exit 1
+	if [ ! -f /media/boot/multiboot/meson64_odroidc2.dtb.linux ]; then
+		today=$(date +%Y%m%d)
+		if [[ "$today" > "20240630" ]]; then
+			echo 1 > $TMPFOLDER/hasError.$$
+			if [ "$LANG_DEP" = "fr" ]; then
+				echo -e ":fg-danger:$HR:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:== ATTENTION Debian 10 Buster n'est officiellement plus supportée depuis le 30 juin 2024, merci de mettre à jour votre distribution !!!:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:== Les dépendances sont bloquées afin d'éviter tout problème, soit $PLUGIN fonctionne et donc on y touche plus tant qu'il tourne, soit il ne fonctionne plus et donc il faut mettre à jour votre distribution.:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:$HR:/fg:" >> $TMPFOLDER/errorLog.$$ 
+			else
+				echo -e ":fg-danger:$HR:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:== WARNING Debian 10 Buster is not supported anymore since June 30, 2024. Please update your distribution!!!:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:== Dependencies are blocked to avoid any issues. Either $PLUGIN works and so we don't touch the dependencies as long as it works, or it doesn't work anymore and you have to update your distribution.:/fg:" >> $TMPFOLDER/errorLog.$$ 
+				echo -e ":fg-danger:$HR:/fg:" >> $TMPFOLDER/errorLog.$$ 
+			fi
+			post
+			exit 1
+		fi
+	else
+		if [ "$LANG_DEP" = "fr" ]; then
+			echo ":fg-warning:$HR:/fg:"
+			echo ":fg-warning:== WARNING == A VERIFIER AU PLUS VITE:/fg:"
+			echo
+			echo ":fg-warning:$HR:/fg:"
+			echo ":fg-warning:== ATTENTION Debian 10 Buster n'est officiellement plus supportée depuis le 30 juin 2024, cependant l'image Debian 11 de la Smart est en cours de finalisation par Jeedom.:/fg:"
+			echo ":fg-warning:== Les dépendances vont quand même se lancer (mais aucun support ne sera fait si celles-ci ne fonctionnent pas !), surveillez les nouvelles de Jeedom afin de mettre à jour en Debian 11 au plus vite quand ils auront sorti leur nouvelle image.:/fg:"
+		else
+			echo ":fg-warning:$HR:/fg:"
+			echo ":fg-warning:== WARNING == TO CHECK SOON:/fg:"
+			echo
+			echo ":fg-warning:$HR:/fg:"
+			echo ":fg-warning:== WARNING Debian 10 Buster is not supported anymore since June 30, 2024. Nevertheless, the Debian 11 image for the Smart box is still under development by Jeedom.:/fg:"
+			echo ":fg-warning:== Dependancies will continue (but no support will be done if it fails). Watch for Jeedom news to update to Debian 11 as soon as they release the new image.:/fg:"
+		fi
+	fi
 fi
 
 step 5 "Mise à jour APT et installation des packages nécessaires"
