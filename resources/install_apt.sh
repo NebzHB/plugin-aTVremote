9861c288b9634cc6276b72c3040019f6068a9ef7
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-09-10 08:31:17 +0200

    use postlund repo v0.15.1

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -103,7 +103,7 @@ step 80 "Installation librairie atvremote"
 VENV_DIR=$BASEDIR/atvremote
 tryOrStop python3 -m venv $VENV_DIR
 tryOrStop $VENV_DIR/bin/python3 -m pip install --no-cache-dir --upgrade pip wheel
-tryOrStop $VENV_DIR/bin/python3 -m pip install --no-cache-dir -I git+https://github.com/NebzHB/pyatv@master
+tryOrStop $VENV_DIR/bin/python3 -m pip install --no-cache-dir -I git+https://github.com/postlund/pyatv@v0.15.1
 
 step 90 "Résumé des packages installés"
 $VENV_DIR/bin/python3 -m pip freeze
