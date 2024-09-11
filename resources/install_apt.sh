0102e23c82445440bfe6aaa71f0fcd3d551be139
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-09-11 11:23:16 +0200

    force upgrade pyatv dependencies

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -40,7 +40,7 @@ step 80 "Installation librairie atvremote"
 VENV_DIR=$BASEDIR/atvremote
 tryOrStop python3 -m venv $VENV_DIR
 tryOrStop $VENV_DIR/bin/python3 -m pip install --no-cache-dir --upgrade pip wheel
-tryOrStop $VENV_DIR/bin/python3 -m pip install --no-cache-dir -I git+https://github.com/NebzHB/pyatv@v0.15.1
+tryOrStop $VENV_DIR/bin/python3 -m pip install --upgrade --no-cache-dir -I git+https://github.com/NebzHB/pyatv@v0.15.1
 
 step 90 "Résumé des packages installés"
 $VENV_DIR/bin/python3 -m pip freeze
